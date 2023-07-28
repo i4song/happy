c55261d1cc208dc03ab790b3c84ac5597186836f
Yongwook Lee <i4song.dev@gmail.com>
2023-07-28 22:44:43 +0900

    :bug: Fix favicon path bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { ViteFaviconsPlugin } from "vite-plugin-favicon";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteFaviconsPlugin({
-      logo: "public/favicon.png",
-      favicons: {
-        path: "assets/",
-      },
-    }),
-  ],
+  plugins: [react()],
 });
